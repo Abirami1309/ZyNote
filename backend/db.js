c7066fd33aa50add9d5882856efa0317c648d19f
Abirami1309 <abiramipandeeswaran@gmail.com>
2026-07-02 15:41:50 +0530

    Environment variables for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,15 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Abirami@13",
-  database: "notes_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
-  if (err) {
-    console.log("Database Connection Failed:", err);
-  } else {
-    console.log("MySQL Connected");
-  }
+  if (err) console.log(err);
+  else console.log("MySQL Connected");
 });
 
 module.exports = db;
